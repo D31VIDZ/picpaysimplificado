59366edfbb688f4dc99901b80c51a0bbcfeb15a7
D31VIDZ <deividzio123@gmail.com>
2023-10-23 18:41:27 -0300

    commit newDto

--- picpaysimplificado/src/main/java/com/example/picpaysimplificado/dots/TransactionDTO.java
+++ picpaysimplificado/src/main/java/com/example/picpaysimplificado/dots/TransactionDTO.java
@@ -1,6 +1,7 @@
 package com.example.picpaysimplificado.dots;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public record TransactionDTO(Long senderId, Long receiverId, BigDecimal amount) {
 	
